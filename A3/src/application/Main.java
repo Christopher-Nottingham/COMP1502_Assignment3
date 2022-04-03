0cae775f21a20523c8964385315f84a7b91ec06e
Christopher-Nottingham <91142333+Christopher-Nottingham@users.noreply.github.com>
2022-04-03 15:49:10 -0600

    Changing FXML

--- A3/src/application/Main.java
+++ A3/src/application/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
@@ -13,12 +15,24 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-		  AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/Sample.fxml"));
+		  
+		  
+		  TabPane root = (TabPane)FXMLLoader.load(getClass().getResource("/view/Untitled.fxml"));
 //			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			
+//		  TabPane tp = new TabPane();
+//		  Tab search = new Tab();
+//		  Tab search1 = new Tab();
+//		  Tab search2 = new Tab();
+//		  
+//		  tp.getTabs().add(search);
+//		  tp.getTabs().add(search2);
+          
+		  Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("/view/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 			
 		} catch(Exception e) {
 			e.printStackTrace();
