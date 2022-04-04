f3dcf5543c84426c181a7e354abaafd637373c10
Christopher-Nottingham <91142333+Christopher-Nottingham@users.noreply.github.com>
2022-04-04 11:17:20 -0600

    Fixing main after merge

--- A3/src/application/Main.java
+++ A3/src/application/Main.java
@@ -13,33 +13,32 @@ import javafx.scene.layout.VBox;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-		  
-		  
-		  TabPane root = (TabPane)FXMLLoader.load(getClass().getResource("/view/Untitled.fxml"));
-//			BorderPane root = new BorderPane();
-			
-//		  TabPane tp = new TabPane();
-//		  Tab search = new Tab();
-//		  Tab search1 = new Tab();
-//		  Tab search2 = new Tab();
-//		  
-//		  tp.getTabs().add(search);
-//		  tp.getTabs().add(search2);
-          
-		  Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("/view/application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	public void start(Stage primaryStage) {    try {
+      
+      
+      TabPane root = (TabPane)FXMLLoader.load(getClass().getResource("/view/Untitled.fxml"));
+//      BorderPane root = new BorderPane();
+        
+//    TabPane tp = new TabPane();
+//    Tab search = new Tab();
+//    Tab search1 = new Tab();
+//    Tab search2 = new Tab();
+//    
+//    tp.getTabs().add(search);
+//    tp.getTabs().add(search2);
+      
+      Scene scene = new Scene(root,400,400);
+        scene.getStylesheets().add(getClass().getResource("/view/application.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        
+    } catch(Exception e) {
+        e.printStackTrace();
+    }
+}
+
+public static void main(String[] args) {
+    launch(args);
+}
 }
